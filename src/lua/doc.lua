beda6a06e3e1291cac55647a09bd7066fdcf56eb
Tim Menzies <tim@menzies.us>
2018-10-27 12:13:21 -0400

    asd

--- src/lua/doc.lua
+++ src/lua/doc.lua
@@ -1,14 +1,20 @@
-DOCS={}
-
-function def(t,   name,fun,doc)
-  for k,v in pairs(t) do
-    if type(v) == "string"
-      then name, doc = k,v
-      else fun = v end end
-   _G[name] = fun
-   DOCS[name] = doc:gsub("^[ \t]+","")
-                   :gsub("\n[^:][ \t]+","\n")
-	           :gsub("\n:","\n")
+do
+  local docs={}
+  local img="https://raw.githubusercontent.com/researchart/re19/master/img/"
+  local img="https://git.io/xxx15"
+  function def(t,   name,fun,doc)
+    for k,v in pairs(t) do
+      if type(v) == "string"
+        then name, doc = k,v
+        else fun = v end end
+    _G[name] = fun
+    doc = doc:gsub("^[ \t]+","")
+             :gsub("\n[^:][ \t]+","\n")
+             :gsub("\n:","\n")
+             :gsub("_IMG%(","![]("..img)
+    print(doc)
+    docs[name] = doc
+  end
 end
 
 def { love = [[# asdasddas
@@ -20,6 +26,9 @@ def { love = [[# asdasddas
     - fereasda
 	      
     ## sdas asda
+
+    _HOME[asdsa]
+    _IMG(adadsasda)
       
     _sdas_  asdad
 	      
@@ -29,8 +38,6 @@ def { love = [[# asdasddas
     function (x) return x+2 end }
 
 
-for k,v in pairs(DOCS) do
-  print(v) end
 
 print(love(20))
 
