8deb03d62d51ef5e57216f94ed959d58a7680405
timm <timm@mac-mini.attlocal.net>
2018-12-23 13:21:15 -0500

    as

--- src/py/dsl2.py
+++ src/py/dsl2.py
@@ -20,7 +20,7 @@ class unittest:
     unittest.tries += 1
     try:
       test()
-    except Exception,e:
+    except Exception as e:
       unittest.fails += 1
       i.report(test)
   def report(i,test):
@@ -32,24 +32,47 @@ isa = isinstance
 sys.dont_write_bytecode = True
 same = lambda x:x
 
-def sorted(x,key=None):
-  x.sort(key=key)
+def order(x,key=lambda z:z):
+  sorted(x, key=key)
   return x
+
 class o:
   """Emulate Javascript's uber simple objects."""
   def has(i)             : return i.__dict__
+  def keys(i)            : return i.has().keys()
   def items(i)           : return i.has().items()
-  def keys(i)            : return [k for k in sorted(i.has().keys(), key=i._order)
-                                   if not k[0] == "_"]
-  def __init__(i,**d)    : i._order=same;  i.has().update(d)
+  def __init__(i,**d)    : i.has().update(d)
   def __setitem__(i,k,v) : i.has()[k] = v
   def __getitem__(i,k)   : return i.has()[k]
-  def __repr__(i)        : return 'o'+str([i(k) for k in i.keys()])
+  def __repr__(i)        : return 'o'+str(i.has())
+
+
+  def items(i)           : return i.__dict__.items()
+  def keys(i)            : return sorted([k for k in order(i.__dict__.keys())])
+  def __init__(i,**d)    : i.__dict__.update(d)
+  def __setitem__(i,k,v) : print(k); i.__dict__[k] = v; print(20)
+  def __getitem__(i,k)   : return i.__dict__[k]
+  #def __repr__(i)        : return 'o'+str([i[k] for k in i.keys() if not k[0] == "_"])
+
+class payload(o):
+  def __init__(i,**d):
+    super().__init__(**d)
+    print(i.cc)
+    for k,v in d.items(): 
+      print(k)
+      #i[v]["name"] = k
+      #i[v]["_rank"] = v.rank()
+  #    print(33)
+ # def keys(i):
+   # return [k for k in order(i.has().items(), key=lambda z:z._rank)]
 
 @ok
 def _o():
   g=o(a=20,z=10,c=100)
-  print(g)
+  print(o.a)
+  o.a= 34
+  print(o.a)
+  print(g.keys())
 
 
 class Has:
@@ -62,8 +85,8 @@ class Has:
     return 0
   def __repr__(i):
     return str(dict(what=i.__class__.__name__,
-                name= i.name,init= i.init,
-                 lo  = i.lo,  hi  = i.hi))
+                    name= i.name,init= i.init,
+                    lo  = i.lo,  hi  = i.hi))
 
 class Flow(Has) :
   def rank(i): return 3
@@ -74,17 +97,18 @@ class Aux(Has)  :
 
 S,A,F = Stock,Aux,Flow
 
+@ok
+def _has():
+  print(payload(cc=F(20), aa=S(10), bb=A(100)))
+  print(22)
+
+sys.exit()
+
 class Model:
   def step(i):
     raise NotImplementedError('"step" must be implemented in subclass')
   def have(i):
     raise NotImplementedError('"have" must be implemented in subclass')
-  def payload(i,state,inits=None):
-    x=o()
-    x._order=lambda z:state[z].rank()
-    if inits:
-      for k,v in inits.keys(): x[k] = v
-    return x
   def state(i):
     tmp=i.have()
     for k,v in tmp.has().items():
@@ -106,8 +130,8 @@ class Model:
 
 class Diapers(Model):
   def have(i):
-    return o(C = S(100), D = S(0),
-             q = F(0),  r = F(8), s = F(0))
+    return things(C = S(100), D = S(0),
+                  q = F(0),  r = F(8), s = F(0))
   def step(i,dt,t,u,v):
     def saturday(x): return int(x) % 7 == 6
     v.C +=  dt*(u.q - u.r)
