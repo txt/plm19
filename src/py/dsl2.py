4827ad23eae84bab10dc968b65eb2f01c149ba4e
Tim Menzies <tim@menzies.us>
2018-12-25 20:08:23 -0500

    as

--- src/py/dsl2.py
+++ src/py/dsl2.py
@@ -1,147 +1,148 @@
-#from __future__ import print_function, division
-import random
-import sys
-import traceback
+#!/usr/bin/env python3
 
-def ok(*lst):
-  print("### ",lst[0].__name__)
-  for one in lst: unittest(one)
-  return one
+"""
+## Utilities
 
-class unittest:
-  tries = fails = 0  #  tracks the record so far
-  @staticmethod
-  def score():
-    t = unittest.tries
-    f = unittest.fails
-    return "# TRIES= %s FAIL= %s %%PASS = %s%%"  % (
-      t,f,int(round(t*100/(t+f+0.001))))
-  def __init__(i,test):
-    unittest.tries += 1
-    try:
-      test()
-    except Exception as e:
-      unittest.fails += 1
-      i.report(test)
-  def report(i,test):
-    print(traceback.format_exc())
-    print(unittest.score(),':',test.__name__)
+Printing stuff.
+"""
 
-r   = random.random
-isa = isinstance
-sys.dont_write_bytecode = True
-same = lambda x:x
+def say(lst):
+  print(', '.join([str(x) for x in lst]))
 
-def order(x,key=lambda z:z):
-  sorted(x, key=key)
-  return x
+"""
+### Associative Arrays
+
+Associative arrays in Python. After creating an `o` with
+
+    x=o(bb=2, aa=0, _cc=2)
+
+then `x`'s fields are avaialble via the _dot_ notation
+
+    print(x.aa) # --> 0
+    x.aa += 10  # adds 10
+    print(x.aa) # --> 10
+
+Also, since this class includes magic methods
+for `__setitem__` and `__getitem__`, we can use
+symbolic access:
+
+    print(x["aa"])
+    x["aa"] *= 10
+    print(x.aa) # --> 100
+
+Also `o` objects know how to print themselves in 
+alphabetical order of their keys.  Further, they don;t
+print "secret" keys (those that start with "`_`".; e.g
+`_cc` in the above). For example, continuing with
+the above value of `x`:
+
+    print(x) ==> o(aa=100, bb=2)
+
+"""
 
 class o:
-  """Emulate Javascript's uber simple objects."""
-#  def has(i)             : return i.__dict__
-#  def keys(i)            : return i.has().keys()
-#  def items(i)           : return i.has().items()
-#  def __init__(i,**d)    : i.has().update(d)
-#  def __setitem__(i,k,v) : i.has()[k] = v
-#  def __getitem__(i,k)   : return i.has()[k]
-#  def __repr__(i)        : return 'o'+str(i.has())
-#
-#
-  def items(i)           : return i.__dict__.items()
-  def keys(i)            : return sorted([k for k in order(i.__dict__.keys())])
-  def __init__(i,**d)    : i.__dict__.update(d)
-  def __setitem__(i,k,v) : print(k); i.__dict__[k] = v; print(20)
-  def __getitem__(i,k)   : return i.__dict__[k]
-  def __repr__(i)        : return i.__class__.__name__+str(
-                                  [i[k] for k in i.keys() if not k[0] == "_"])
-
-#class payload(o):
-#  def __init__(i,**d):
-#    i.__dict__.update(d)
-#    for k,v in d.items(): 
-#      v.name = k
-#  def keys(i):
-#    return [k for k in sorted(i.__dict__.keys(), key=lambda z:i[z].rank())]
-#  def slots(i,old=dict()):
-#    out = o({k:i[k].init for k in i.keys()})
-#    for k,v in old.items():
-#        out[k] = v
-#    return out
-#
-#vrry round and old and enw item
-
-# we coudl the meta knowledge
-# need to keys in sorted rank order
-# need values
-class Have:
-  def __init__(i,**d):
+  def __init__(i,**d): 
     i.__dict__.update(d)
-    i._keys = [k for k in sorted(d.keys(), key=lambda z:d[z].rank())]
-    for pos,k in enumerate(i.keys()):
-        d[k].pos = pos
-        d[k].value= d[k].init
-  def keys(i)  : return i._keys
-  def values(i): return i._values
-  def clone(i,inits=None):
-    out = Have({k:v.clone() for k,v in i.items})
-    for k,v in inits.items():
-        out[k] = v # but not reallly
+  def items(i): 
+    return i.__dict__.items()
+  def keys(i): 
+    return sorted(list(i.__dict__.keys()))
+  def __setitem__(i,k,v):
+    i.__dict__[k] = v
+  def __getitem__(i,k): 
+    return i.__dict__[k]
+  def __repr__(i): 
+    tmp = ['%s=%s' % (k, i[k]) 
+           for k in i.keys() if k[0]  != "_"]
+    return 'o('+', '.join(tmp) + ')'
 
-  def __setitem__(i,k,v): 
-     i[k].value = i[k].restrain(v)
+"""
+## `Stock`s, `Flow`, `Aux`illary Variables
 
-class Has:
+In compartmental modelling:
+    
+- `Stock`s store quantities;
+- `Flow`s dribble quanities between `Stock`s;
+- `Aux`illary variabes contain business logic.
+
+For our purposes, all of these are nearly the same so we represent
+them all as `Thing`s. `Stock, Flow, Aux` are subclasses of `Thing`.
+All these `Thing`s have different `rank`s (so when we print a
+list of `Thing`s, we can sort them out
+into all the `Stock`s, before all the `Aux`s before all the 
+`Flow`s.
+
+Also, `Things` restrain themselves to always be between 
+a `lo` and `hi` value.
+
+"""
+
+class Thing:
   def __init__(i,init=None,lo=0,hi=100):
     i.init = init if init != None else lo
     i.lo,i.hi = lo,hi
-    i.pos=0
+    i.name = 0,''
   def restrain(i,x):
     return max(i.lo, min(i.hi, x))
-  def rank(i): 
-    return 0
-  def clone(i):
-    return i.__class__(init=i.init,lo=i.lo,hi=i.hi)
-  def __repr__(i):
-    return str(dict(what=i.__class__.__name__,
-                    name= i.name,init= i.init,
-                    lo  = i.lo,  hi  = i.hi))
+  def rank(i):
+    raise NotImplementedError(
+             '"rank" must be implemented in subclass')
 
-class Flow(Has) : 
+class Flow(Thing) : 
   def rank(i): return 3
-class Stock(Has): 
+class Stock(Thing): 
   def rank(i): return 1
-class Aux(Has)  : 
+class Aux(Thing)  : 
   def rank(i): return 2
+
+# Here some short hand for quickly specifying `Stock`s,`Aux`s, `Flow`s.
 
 S,A,F = Stock,Aux,Flow
 
-@ok
-def _has():
-  g=Have(dd=F(100),cc=F(20), aa=S(10), bb=A(100))
-  print(g.keys())
-  print(g.values())
+"""
+`Things` stores out meta-knowledge about variables in a model.
+The call
+Have(C = S(100), D = S(0),
+                q = F(0),  r = F(8), s = F(0))
 
-sys.exit()
+"""
+class Things:
+  def __init__(i,**things):
+    i.things = things
+    i.order = [k for k in sorted(i.things.keys(), 
+               key=lambda z:i.things[z].rank())]
+    for k in i.order:
+      i.things[k].name = k
+  def payload(i,old=None):
+    out = o(**{k:(i.things[k].init) for k in i.order})
+    if old:
+      for k,v in old.items():
+        out[k] = i.things[k].restrain(v)
+    return out
+  def asList(i,d):
+    return [d[k] for k in i.order]
 
 class Model:
   def step(i):
-    raise NotImplementedError('"step" must be implemented in subclass')
+    raise NotImplementedError(
+             '"step" must be implemented in subclass')
   def have(i):
-    raise NotImplementedError('"have" must be implemented in subclass')
+    raise NotImplementedError(
+             '"have" must be implemented in subclass')
   def run(i,dt=1,tmax=30):
     have = i.have()
-    t,b4  = 0, i.values()
-    print(have.keys())
+    t,b4  = 0, i.have().payload()
+    say( ['t'] + i.have().order )
     while t < tmax:
-      now = i.have.slots(b4)
+      now = i.have().payload(b4)
       i.step(dt,t,b4,now)
-      print(now)
+      say( [t] + i.have().asList(now) )
       t += dt
       b4 = now
 
 class Diapers(Model):
   def have(i):
-    return payload(C = S(100), D = S(0),
+    return Things(C = S(100), D = S(0),
                   q = F(0),  r = F(8), s = F(0))
   def step(i,dt,t,u,v):
     def saturday(x): return int(x) % 7 == 6
@@ -152,7 +153,6 @@ class Diapers(Model):
     if t == 27: # special case (the day i forget)
       v.s = 0
 
-@ok
-def _diapers1():
-  printm(Diapers().run())
+if __name__ == "__main__":
+    Diapers().run()
 
