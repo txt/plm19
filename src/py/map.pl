abfb130772940d3cdf107d5b8cfd9afc2f063845
tim.menzies <tim.menzies@gmail.com>
2019-01-09 01:12:30 -0500

    as

---
+++ src/py/map.pl
@@ -0,0 +1,26 @@
+color(red).
+color(green).
+color(blue).
+color(yellow).
+
+neighbor(StateAColor, StateBColor) :- 
+    color(StateAColor), 
+    color(StateBColor), 
+    not(StateAColor = StateBColor). /* \= is the not equal operator */
+
+germany([SH, MV, HH, HB, NI, ST, BE, BB, SN, NW, HE, TH, RP, SL, BW, BY]) :- 
+neighbor(SH, NI), neighbor(SH, HH), neighbor(SH, MV),
+neighbor(HH, NI),
+neighbor(MV, NI), neighbor(MV, BB),
+neighbor(NI, HB), neighbor(NI, BB), neighbor(NI, ST), neighbor(NI, TH),
+neighbor(NI, HE), neighbor(NI, NW),
+neighbor(ST, BB), neighbor(ST, SN), neighbor(ST, TH),
+neighbor(BB, BE), neighbor(BB, SN),
+neighbor(NW, HE), neighbor(NW, RP),
+neighbor(SN, TH), neighbor(SN, BY),
+neighbor(RP, SL), neighbor(RP, HE), neighbor(RP, BW),
+neighbor(HE, BW), neighbor(HE, TH), neighbor(HE, BY),
+neighbor(TH, BY),
+neighbor(BW, BY).
+
+:- bagof(X,germany(X),L), length(L,N), print(N).
