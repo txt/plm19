3b96e23ae7e6537893fba256ca2c46fb34c82958
tim.menzies <tim.menzies@gmail.com>
2019-01-18 21:57:38 -0500

    saving

---
+++ src/pl/grammar.pl
@@ -0,0 +1,22 @@
+sentence -->
+  subject,
+  verb,
+  object.
+
+subject -->
+  modifier,
+  noun.
+
+object -->
+  modifier,
+  noun.
+
+modifier --> [the].
+
+noun --> [cat].
+noun --> [mouse].
+noun --> [polar, bear].
+
+verb --> [chases].
+verb --> [eats].
+
